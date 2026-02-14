009ec6e147de67532830434010c1042e4822c47b
channa-sugathadasa <channavimukthi21871@gmaail.com>
2026-02-15 01:33:40 +0530

    init ✨

--- portfoliyo/src/Components/Intro/intro.js
+++ portfoliyo/src/Components/Intro/intro.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './intro.css'
 import bg from '../../assets/image.png';
 import { Link } from 'react-scroll'
+import btnImage from '../../assets/hireme.png'
 
 const Intro = () => {
   return (
         <section id="intro">
-            <div className="introcontent">
+            <div className="introContent">
                 <spam className="hello">Hello</spam>
                 <spam className="introText">I'm </spam><spam className="introName">Channa Sugathadasa </spam> <br /> <spam> Web Site Designer </spam>
-                <p className='intropara'>I'a skilled Web designer with experience in creating visually appeling and user friendly websites. </p>
-                <Link><button className='btn'><img src="" alt=''/>Hire Me</button></Link>
+                <p className='introPara'>I'a skilled Web designer with experience in creating <br/> visually appeling and user friendly websites. </p>
+                <Link><button className='btn'><img src={btnImage} alt="Hire me" className='btnImg'/>Hire Me</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>  
