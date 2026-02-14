62e7905db866a5cc6487cbcece5ea996e1985be2
channa-sugathadasa <channavimukthi21871@gmaail.com>
2026-02-15 03:29:22 +0530

    add footer component and update logo dimensions ✏️

--- portfoliyo/src/App.js
+++ portfoliyo/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/NavBar/navbar";
 import Intro from "./Components/Intro/intro"; 
+import Footer from "./Components/Footer/footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Footer />
     </div>
   );
 }
